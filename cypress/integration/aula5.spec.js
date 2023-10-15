007f5bd499c3280b36e534044fceeb5ce2cb9fe3
Maria Tereza <mtereza.cl@gmail.com>
2023-10-15 17:11:34 -0300

    atualizacao nos testes

--- cypress/integration/aula5.spec.js
+++ cypress/integration/aula5.spec.js
@@ -1,42 +1,30 @@
 /// <reference types="cypress" />
 
+import { faker } from '@faker-js/faker';
+
 describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
-        E validando minha compra ao final */
 
     beforeEach(() => {
         cy.visit('produtos/')
     });
 
-    it('Deve ir para segunda pagina de produtos', () => {
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 4
+
+        let emailFaker = faker.internet.email()
+        let nomeFaker = faker.person.firstName()
+        let sobrenomeFaker = faker.person.lastName()
+       
         cy.get(':nth-child(2) > .page-numbers')
             .click()
 
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(3)').should('contain', 'Página 2')
-    });
 
-    it('Deve selecionar um produto da lista', () => {
-        cy.get(':nth-child(2) > .page-numbers')
-            .click()
         cy.get('[class="product-block grid"]')
             .eq(4)
             .click()
 
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(8)').should('contain', 'Atlas Fitness Tank')
-    });
-
-    it('Deve adicionar um produto ao carrinho', () => {
-        var quantidade = 4
-        cy.get(':nth-child(2) > .page-numbers')
-            .click()
-        cy.get('[class="product-block grid"]')
-            .eq(4)
-            .click()
 
         cy.get('.button-variable-item-S').click()
         cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
@@ -61,16 +49,32 @@ describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade + quantidade)
 
-    });
-
-    it('Deve ver carrinho e concluir a compra', () => {
         cy.get('.woocommerce-message > .button')
             .click()
         cy.get('.breadcrumb > .active').should('contain', 'Carrinho')
 
         cy.get('.checkout-button')
             .click()
-        cy.get('.page-title').should('contain', 'CHECKOUT')
-    })
+        cy.get('.breadcrumb > .active').should('contain', 'Checkout')
+
+        cy.get('#billing_first_name_field > label').type(nomeFaker)
+        cy.get('#billing_last_name').type(sobrenomeFaker)
+        cy.get('#select2-billing_country-container')
+        cy.get('#billing_address_1').type('Rua Almirante')
+        cy.get('#billing_city').type('Rio de Janeiro')
+        cy.get('#select2-billing_state-container')
+        cy.get('#billing_postcode').type('71458-999')
+        cy.get('#billing_phone').type('9999-9999')
+        cy.get('#billing_email').type(emailFaker)
+        cy.get('#createaccount').click()
+        cy.get('#account_password').type('123456789')
+        cy.get('#payment_method_cod').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+    });
+
 
 })
